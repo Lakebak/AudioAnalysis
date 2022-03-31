0eb52127b0baa08ed1c7c3794d461e96cf8daf1e
Lakebak <nicola.corbellini@edu.unige.it>
2022-03-31 18:41:49 +0200

    estrattore feature

--- denoise_audio.py
+++ denoise_audio.py
@@ -7,12 +7,12 @@ from modules import Audio
 def remove_noise(file):
     audio = Audio(file, sr=44100)
     signal = audio.load()
-    audio.denoise(signal=signal, save=True, out_path='/mnt/f/PhD/_Segments/Segmented15s/.selected_audio/ami')
+    audio.denoise(signal=signal, save=True, out_path='/mnt/f/PhD/_Segments/Segmented15s/.selected_audio/ami', prop_decrease=0.8)
 
-ami_path = "/mnt/f/PhD/_Segments/44kHz/15s/ami"
+path = "/mnt/f/PhD/_Segments/44kHz/15s/ami"
 
 print("Collecting...")
-holistic_files = [file for file in glob.glob(os.path.join(ami_path, "*.*")) if fnmatch.fnmatch(file, "*Mix-Headset*")]
+holistic_files = [file for file in glob.glob(os.path.join(path, "*.*")) if fnmatch.fnmatch(file, "*Mix-Headset*")]
 
 print("Done.\nStarting parallel...")
 p_map(remove_noise, holistic_files)
--- modules/Features.py
+++ modules/Features.py
@@ -1,6 +1,11 @@
+from operator import index
+import os
+import numpy as np
 import pandas as pd
-from pyAudioAnalysis import ShortTermFeatures, MidTermFeatures
-from Audio import Audio
+from modules import Audio
+from modules.pyAudioAnalysis.MidTermFeatures import mid_feature_extraction
+from modules.pyAudioAnalysis.ShortTermFeatures import feature_extraction
+from pyannote.audio.pipelines import VoiceActivityDetection, OverlappedSpeechDetection
 
 
 class AudioFeature:
@@ -11,8 +16,95 @@ class AudioFeature:
 
     class TurnTaking:
 
-        def __init__(self) -> None:
-            pass
+        def __init__(self, 
+                    vad_onset=0.648,
+                    vad_offset=0.577,
+                    vad_min_dur_on=0.181,
+                    vad_min_dur_off=0.037,
+                    ovd_onset=0.448,
+                    ovd_offset=0.362,
+                    ovd_min_dur_on=0.116,
+                    ovd_min_dur_off=0.187):
+            self.vad_onset = vad_onset
+            self.vad_offset = vad_offset
+            self.vad_min_dur_on = vad_min_dur_on
+            self.vad_min_dur_off = vad_min_dur_off
+            self.ovd_onset = ovd_onset
+            self.ovd_offset = ovd_offset
+            self.ovd_min_dur_on = ovd_min_dur_on
+            self.ovd_min_dur_off = ovd_min_dur_off
+
+        def __make_df(path, iterable_track):
+            file_name = os.path.splitext(os.path.basename(path))[0]
+
+            data = {}
+            
+            for tracks, person, _ in iterable_track.itertracks(yield_label=True):
+                #data['File'] = file_name
+                data['Person'] = person
+                data['Start'] = tracks.start
+                data['End'] = tracks.end
+                data['Duration'] = tracks.duration
+                data['Middle'] = tracks.middle
+
+            df = pd.DataFrame([data], index=[file_name])
+            
+
+            if df.empty:
+                data = {'Person': np.nan,
+                'Start': np.nan,
+                'End': np.nan,
+                'Duration': np.nan,
+                'Middle': np.nan
+                }
+                df = pd.DataFrame([data], index=[file_name])
+                
+            #df.set_index('File', inplace=True)
+            return df
+
+        def __call__(self, path):
+                        
+            def voice_activity(self, path):
+                pipeline = VoiceActivityDetection(segmentation='pyannote/segmentation')
+                hyper_parameters = {
+                    "onset": self.vad_onset,
+                    "offset": self.vad_offset,
+                    "min_duration_on": self.vad_min_dur_on,
+                    "min_duration_off": self.vad_min_dur_off
+                }
+
+                pipeline.instantiate(hyper_parameters)
+
+                return pipeline(path)
+
+            def overlapped_speech(self, path):
+                pipeline = OverlappedSpeechDetection(segmentation='pyannote/segmentation')
+                hyper_parameters = {
+                    "onset": self.ovd_onset,
+                    "offset": self.ovd_offset,
+                    "min_duration_on": self.ovd_min_dur_on,
+                    "min_duration_off": self.ovd_min_dur_off
+                }
+
+                pipeline.instantiate(hyper_parameters)
+
+                return pipeline(path)
+
+            vad = voice_activity(self, path)
+            ovd = overlapped_speech(self, path)
+            df_vad = AudioFeature.TurnTaking.__make_df(path, vad)
+            df_ovd = AudioFeature.TurnTaking.__make_df(path, ovd)
+
+            return df_vad, df_ovd
+
+        def make_dataset(self, df_vad, df_ovd):
+            dataset = df_vad.join(df_ovd,
+            lsuffix='_vad',
+            rsuffix='_ovd',
+            sort=True)
+
+            return dataset
+
 
     class Prosody:  # TO-DO: implementare possibilità di scegliere le feature da un file di configurazione
 
@@ -47,7 +139,7 @@ class AudioFeature:
                 def low_level():
                     if low_lvl:
                         low_feat, low_feat_names = \
-                            ShortTermFeatures.feature_extraction(signal=signal,
+                            feature_extraction(signal=signal,
                                                                  sampling_rate=sampling_rate,
                                                                  window=window,
                                                                  step=step)
@@ -61,7 +153,7 @@ class AudioFeature:
                 def mid_level():
                     if mid_lvl:
                         mid_feat, _, mid_feat_names = \
-                            MidTermFeatures.mid_feature_extraction(signal=signal,
+                            mid_feature_extraction(signal=signal,
                                                                    sampling_rate=sampling_rate,
                                                                    mid_window=mid_window,
                                                                    mid_step=mid_step,
