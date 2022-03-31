65216dda486fb5090319477283b6d040470748c0
Lakebak <nicorb932@hotmail.com>
2022-03-31 21:43:09 +0200

    post processare turntaking

--- modules/Features.py
+++ modules/Features.py
@@ -14,6 +14,8 @@ class AudioFeature:
         self.turn_taking = self.TurnTaking()
         self.prosody = self.Prosody()
 
+    # Questa funzione deve essere implementata - controllare il join
+    # perchè hanno campioni diversi
     def __call__(self,
                  turn_taking_df,
                  prosody_df):
@@ -108,8 +110,8 @@ class AudioFeature:
                                   lsuffix='_vad',
                                   rsuffix='_ovd',
                                   sort=True)
-
             return dataset
+
 
     class Prosody:  # TO-DO: implementare possibilità di scegliere le feature da un file di configurazione
 
